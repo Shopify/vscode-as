4100a7ef078fd9cb31aeb29d5bb2f48907e5ff56
Saúl Cabrera <saulecabrera@gmail.com>
2020-09-10 15:34:56 -0400

    feat: Rewrite Command with Maybe
    This commit rewrites the Command module to make use of
    purify-ts/Maybe.
    
    Command from context will take a look at the extension context in order to
    determine which asls installation to use, in all cases it fallback to the system's
    installation.

--- src/Command.ts
+++ src/Command.ts
@@ -1,18 +1,33 @@
 import * as vscode from 'vscode';
+import { Maybe } from 'purify-ts/Maybe';
+import * as shell from 'shelljs';
 
 export type Command = string;
 
 /**
  * Returns the language server command from the extension context.
  *
- * @param context - the extension context
+ * If the extension is on development mode, the local symlinked
+ * binary path will be returned.
+ * 
+ * If the extension is on production mode, the global binary path
+ * will be returned.
+ *
+ * @param ctx - the extension context
  * @returns the language server command
  */
-export const fromContext = (context: vscode.ExtensionContext): Command => {
-  switch(context.extensionMode) {
+export const fromContext = (ctx: vscode.ExtensionContext): Maybe<Command> => {
+  const fallback = Maybe
+    .fromNullable(shell.which('asls'))
+    .map(cmd => cmd.toString());
+
+  switch(ctx.extensionMode) {
     case vscode.ExtensionMode.Development:
-      return context.asAbsolutePath('asls');
+      return Maybe
+        .fromNullable(shell.which(ctx.asAbsolutePath('asls')))
+        .map(cmd => cmd.toString())
+        .alt(fallback);
     default:
-      return 'asls';
+      return fallback;
   }
 };
