a9d791a6b320c17125f4b4c1e54383bc69cf7317
Saúl Cabrera <saulecabrera@gmail.com>
2021-04-29 09:16:13 -0400

    feat: Add a TypeScript plugin for AS

---
+++ asts/src/index.ts
@@ -0,0 +1,30 @@
+import * as ts from 'typescript/lib/tsserverlibrary';
+import * as mm from 'minimatch';
+import * as path from 'path';
+
+let defaultConfig = {include: "assembly/**/*.ts", rootPath: ''};
+
+const create = ({languageService}: ts.server.PluginCreateInfo): ts.LanguageService => ({
+  ...languageService,
+  getCompilerOptionsDiagnostics: () => [],
+  getSyntacticDiagnostics: filter(languageService, "getSyntacticDiagnostics"),
+  getSemanticDiagnostics: filter(languageService, "getSemanticDiagnostics"),
+  getSuggestionDiagnostics: filter(languageService, "getSuggestionDiagnostics"),
+});
+
+const init = (): ts.server.PluginModule => ({
+  create,
+  onConfigurationChanged: (config) => {
+    defaultConfig = config;
+  }
+});
+
+const filter = (service: ts.LanguageService, key: keyof ts.LanguageService) => (filename: string) => {
+  const relative = path.relative(defaultConfig.rootPath, filename);
+  if (mm(relative, defaultConfig.include)) {
+    return [];
+  }
+  return (service as any)[key](filename);
+};
+
+export = init;
